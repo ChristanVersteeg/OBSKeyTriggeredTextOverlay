47c47cf6795181111cc36d98f537c7e8809f32a2
Christan Versteeg <90323125+ChristanVersteeg@users.noreply.github.com>
2023-11-18 23:52:41 +0100

    Initial commit

---
+++ OBSKeyTriggeredTextOverlayScripts/InteractiveLabelManager.py
@@ -0,0 +1,97 @@
+import tkinter as tk
+from tkinter import font
+import keyboard as key
+import json
+import os
+
+def save_position(position):
+    open(position_file, "w").write(f"{position[0]},{position[1]}")
+
+def load_position():
+    return list(map(int, open(position_file, "r").read().split(',')))
+
+def file_to_app_data_path(file):
+   return os.path.join(os.environ['LOCALAPPDATA'], 'OBSKeyTriggeredTextOverlayConfig', file)
+
+json_file = file_to_app_data_path('label_settings.json')
+with open(json_file, 'r') as file:
+    settings = json.load(file)
+    text = settings['text']
+    fg_color = settings['fg_color']
+    bg_color = settings['bg_color']
+    font_size = settings['font_size']
+    font_family = settings['font_family']
+    font_weight = settings['font_weight']
+    font_slant = settings['font_slant']
+    font_overstrike = settings['font_overstrike']
+    font_underline = settings['font_underline']
+    border_size = settings['border_size']
+    relief_type = settings['relief_type']
+    alpha = settings['alpha']
+    hotkey = settings['hotkey']
+    window_name = settings['window_name']
+
+position_file = file_to_app_data_path('window_position.txt')
+if not os.path.exists(os.path.dirname(position_file)): os.makedirs(os.path.dirname(position_file))
+if not os.path.exists(position_file): open(position_file, 'w').write("0,0")
+position = load_position()
+old_position = position
+warn_window = None
+
+def create_tkinter_loop():
+    root = tk.Tk()
+    root.withdraw()
+    root.mainloop()
+
+def create_label():
+    window = tk.Toplevel()
+    window.title(window_name)
+    window.overrideredirect(True)
+    window.attributes('-topmost', True)
+    window.geometry(f"+{position[0]}+{position[1]}")
+    window.attributes('-alpha', alpha)
+    label_font = font.Font(family=font_family, size=font_size, weight=font_weight, slant=font_slant, overstrike=font_overstrike, underline=font_underline)
+    label = tk.Label(window, text=text, bg=bg_color, fg=fg_color, borderwidth=border_size, relief=relief_type, font=label_font)
+    label.pack()
+    
+    return window, label
+
+def label_movement(window, label):
+    window.drag_position = None
+    
+    def on_press_release(event):
+        global position
+        window.drag_position = (event.x, event.y) if window.drag_position is None else None
+        position = window.winfo_x(), window.winfo_y()
+        
+    def on_move(event):
+        deltax, deltay = event.x - window.drag_position[0], event.y - window.drag_position[1]
+        window.geometry(f"+{window.winfo_x() + deltax}+{window.winfo_y() + deltay}")
+
+    label.bind('<ButtonPress-1>', on_press_release)
+    label.bind('<ButtonRelease-1>', on_press_release)
+    label.bind('<B1-Motion>', on_move)
+    
+def make_label_window():
+    window, label = create_label()
+    
+    label_movement(window, label)
+
+    return window
+
+def toggle_window():
+    global warn_window
+    global old_position
+    
+    if warn_window is None:
+        warn_window = make_label_window()
+    else:
+        warn_window.destroy()
+        warn_window = None
+        if(old_position is not position):
+            print(old_position, position)
+            save_position(position)
+            old_position = position
+key.add_hotkey(hotkey, toggle_window)
+
+create_tkinter_loop()
---
+++ README.md
@@ -0,0 +1,24 @@
+# OBSKeyTriggeredTextOverlay
+A tool that creates a window with text upon a hotkey press. This window will always be on top of everything. Made by Wumpie.
+
+The actual commit history is on my Python Projects Repository
+From commit ChristanVersteeg/python_projects@f68f81a to ChristanVersteeg/python_projects@f8c1b28
+
+# Latest Release: https://github.com/ChristanVersteeg/OBSKeyTriggeredTextOverlay/releases
+
+# Setup
+1. Download Python 3.11.6. Make sure that it's added to your PATH. Python's 3.11.6 installer (for Windows) is given with this release for ease of acess.
+2. If you do not have OBS yet, download the latest release (30.0.0 as of release) https://obsproject.com/download
+3. Open your OBS, then `Tools > Scripts`, within that menu, click `Python Settings`, from there click browse and assigned your installed Python 3.11.6.
+![image](https://github.com/ChristanVersteeg/OBSKeyTriggeredTextOverlay/assets/90323125/bc5d2e2f-e691-4aea-b24f-4438eed3eaa2)
+4. Download the Python files; `InteractiveLabelManager.py` and `OBSKeyTriggeredTextOverlay.py`. They're both within the `OBSKeyTriggeredTextOverlayScripts.zip` folder brought with this release.
+5. Store these in a place where you want them, you'll always need these two to run the program. 
+6. Then go back to `Tools > Scripts`, within the menu click `Scripts`, then click the plus icon in the bottom left, and browse to the downloaded OBSKeyTriggeredTextOverlay.py and add it to the list.
+7. Once it's added to the list you should get a view like this:
+![image](https://github.com/ChristanVersteeg/OBSKeyTriggeredTextOverlay/assets/90323125/8fd37afe-fbfe-4f5e-8364-465a01532a96)
+8. If the first field (Python Script Path) has not assigned itself, click the browse button, and assign it the InteractiveLabelManager.py you've downloaded to the field.
+9. That's it! Now you should be able to open the text overlay with the key `9`, if you prefer to assign another hotkey, you can do that by just clicking the hotkey dropdown and choosing a key of your liking. You can also move around the text overlay by just holding left mouse button on it and dragging. It will save it's last position that it was in.
+10. You can tinker with the rest of the settings to get the text as you need. Don't forget to press the refresh button bottom left of the scripts window when you changed the settings, else the new settings won't be applied to the text overlay.
+
+### Data
+Config files are stored within your appdata, in the folder `<Disc>:\Users\<UserName>\AppData\Local\OBSKeyTriggeredTextOverlayConfig`. Note, changing the `.json` file directly will not work, OBS will override it. The settings in OBS are leading. You can change the `window_position.txt` manually if you really wanted to. It will use what you changed it to upon script refresh. Note that if you do not adhere to the formatting of the `window_position.txt` it will break. The json will be fine since OBS overrides it, always. If you delete the config files they will be regenerated again.
